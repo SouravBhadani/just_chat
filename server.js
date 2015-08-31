339f476cdcb3f12b7e19cad505ff61a500728eee
sourav bhadani <souravbhadani94@gmail.com>
2015-08-31 20:19:05 +0530

    socket changes for connecting to rails

--- server.js
+++ server.js
@@ -5,7 +5,8 @@
 //
 var http = require('http');
 var path = require('path');
-
+var redis = require('redis');
+var redis_client = redis.createClient(); //creates a new client
 var async = require('async');
 var socketio = require('socket.io');
 var express = require('express');
@@ -24,61 +25,105 @@ router.use(express.static(path.resolve(__dirname, 'client')));
 var messages = [];
 var sockets = [];
 
-io.on('connection', function (socket) {
+
+//pub_sub example
+var pub_sub = {
+  events: {},
+  add_socket: function (user_id, socket) {
+    this.events[user_id] = this.events[user_id] || [];
+    this.events[user_id].push(socket);
+  },
+  remove_socket: function(user_id, socket) {
+    if (this.events[eventName]) {
+      for (var i = 0; i < this.events[eventName].length; i++) {
+        if (this.events[eventName][i] === fn) {
+          this.events[eventName].splice(i, 1);
+          break;
+        }
+      };
+    }
+  },
+  emit: function (eventName, data) {
+    if (this.events[eventName]) {
+      this.events[eventName].forEach(function(fn) {
+        fn(data);
+      });
+    }
+  }
+};
+
+
+
+
+
+  redis_client.on('connect', function() {
+      console.log('connected');
+  });
+
+  io.on('connection', function (socket) {
+    
     messages.forEach(function (data) {
       socket.emit('message', data);
     });
 
-    sockets.push(socket);
+    redis_client.sadd('unknown',socket.id , function(err, reply) {
+      console.log(reply); // 3
+    });
+    
+    socket.on('connect_me', function (data) {
+      console.log(data);
+    });
 
+
+    sockets.push(socket);
     socket.on('disconnect', function () {
       sockets.splice(sockets.indexOf(socket), 1);
-      updateRoster();
+     // updateRoster();
     });
 
-    socket.on('message', function (msg) {
-      var text = String(msg || '');
+    // socket.on('message', function (msg) {
+    //   var text = String(msg || '');
 
-      if (!text)
-        return;
+    //   if (!text)
+    //     return;
 
-      socket.get('name', function (err, name) {
-        var data = {
-          name: name,
-          text: text
-        };
+    //   // socket.get('name', function (err, name) {
+    //   //   var data = {
+    //   //     name: name,
+    //   //     text: text
+    //   //   };
 
-        broadcast('message', data);
-        messages.push(data);
-      });
-    });
+    //   //   broadcast('message', data);
+    //   //   messages.push(data);
+    //   // });
+    // });
 
     socket.on('identify', function (name) {
-      socket.set('name', String(name || 'Anonymous'), function (err) {
-        updateRoster();
-      });
+      // socket.set('name', String(name || 'Anonymous'), function (err) {
+      //   updateRoster();
+      // });
     });
   });
 
-function updateRoster() {
-  async.map(
-    sockets,
-    function (socket, callback) {
-      socket.get('name', callback);
-    },
-    function (err, names) {
-      broadcast('roster', names);
-    }
-  );
-}
+// function updateRoster() {
+//   async.map(
+//     sockets,
+//     function (socket, callback) {
+//       socket.get('name', callback);
+//     },
+//     function (err, names) {
+//       broadcast('roster', names);
+//     }
+//   );
+// }
 
-function broadcast(event, data) {
-  sockets.forEach(function (socket) {
-    socket.emit(event, data);
-  });
-}
+// function broadcast(event, data) {
+//   sockets.forEach(function (socket) {
+//     socket.emit(event, data);
+//   });
+// }
 
-server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+server.listen(process.env.PORT || 5000, process.env.IP || "0.0.0.0", function(){
   var addr = server.address();
   console.log("Chat server listening at", addr.address + ":" + addr.port);
 });
